ae240f0d6b41a61dd87cf29e2609a8309d7d2d0c
mauroviniciussilva <mauroviniciussilva@gmail.com>
2020-08-31 08:14:32 -0300

    fix: adding validation

--- README.md
+++ README.md
@@ -12,15 +12,17 @@ npm install read-csv-as-array --save
 
 ## How to use
 
-You only need to pass your file and the separator for it to work. But, if the first line of your csv, for some reason, is not the *header* (the line that contains the keys that will be used to create the objects), you can pass the number of lines that you want to remove from the beggining of the file.
+You only need to pass your file for it to work. But, if the first row of your csv, for some reason, is not the *header* (the row that contains the keys that will be used to create the object), you can pass the number of rows that you want to remove from the beggining of the file.
+
+By default, the separator is the comma (`,`), but you can specify others (depending on the file you're going to convert you may use `;`).
 
 ```JS
 import { convertCsvToArray } from "read-csv-as-array";
 
-const array = await convertCsvToArray(file, separator, linesToRemove);
+const array = await convertCsvToArray(file, separator, rowsToRemove);
 ```
 
-### Example of input and output:
+### Example of input/output:
 
 #### Input
 
@@ -47,3 +49,19 @@ const array = convertCsvToArray(file, ";");
 ]
 ```
 
+> **Note!**
+> If there is any white space in the header, it'll be replaced by an underscore.
+
+## Helper Method
+
+The conversion method already performs file type validation, however, if you want to take any action when identifying a different type of file you can use the `isCsvExtension` method, which returns a boolean:
+
+```JS
+import { isCsvExtension } from "read-csv-as-array";
+
+const isCsv = await isCsvExtension(file);
+```
+
+---
+
+If you spot any errors or have suggestions for improvement, feel free to [open an issue](https://github.com/mauroviniciussilva/read-csv-as-array/issues)!
--- index.js
+++ index.js
@@ -13,35 +13,49 @@ async function processFile(file) {
   try {
     return await readFileAsync(file);
   } catch (err) {
-    console.log(err);
+    throw "File extension is not valid";
   }
 }
 
-export async function convertCsvToArray(file, separator = ",", linesToRemove = 0) {
-  const csv = await processFile(file);
-  const lines = csv.split("\n");
-  if (linesToRemove > 0) {
-    for (var i = 0; i < linesToRemove; i++) {
-        lines.shift();
+export function isCsvExtension(file) {
+  let isCsv = true;
+  if (!file || !file.name) isCsv = false;
+  if (file.name.split(".").pop() != "csv") isCsv = false;
+  return isCsv;
+}
+
+export async function convertCsvToArray(file, separator = ",", rowsToRemove = 0) {
+  try {
+    const isValid = isCsvExtension(file);
+    if (!isValid) throw "File extension is not valid";
+
+    const csv = await processFile(file);
+    const rows = csv.split("\n");
+    if (rowsToRemove > 0) {
+      for (var i = 0; i < rowsToRemove; i++) {
+        rows.shift();
+      }
     }
-  }
-  const result = [];
-  const headers = lines[0].split(separator);
+    const array = [];
+    const headers = rows[0].split(separator);
 
-  for (var i = 1; i < lines.length; i++) {
-    const obj = {};
-    const currentline = lines[i].split(separator);
+    for (var i = 1; i < rows.length; i++) {
+      const obj = {};
+      const currentRow = rows[i].split(separator);
 
-    for (var idx = 0; idx < headers.length; idx++) {
-      const key = headers[idx].trim();
-      const value = currentline[idx]
-        ? currentline[idx].replace(/['"]+/g, "")
-        : null;
-      obj[key] = value;
+      for (var idx = 0; idx < headers.length; idx++) {
+        const key = headers[idx].trim().replace(" ", "_");
+        const value = currentRow[idx]
+          ? currentRow[idx].replace(/['"]+/g, "")
+          : null;
+        obj[key] = value;
+      }
+
+      array.push(obj);
     }
 
-    result.push(obj);
+    return array;
+  } catch (err) {
+    throw "Unable to read the file";
   }
-
-  return result;
 }
